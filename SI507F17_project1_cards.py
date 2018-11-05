b824553bfdd5ef9fae728111001716050ebfa32d
周依弦 <zyx@zhouyixiandeMacBook-Pro.local>
2018-11-05 10:24:32 -0500

    Person1: changed __str__ return value

--- SI507F17_project1_cards.py
+++ SI507F17_project1_cards.py
@@ -21,7 +21,7 @@ class Card(object):
         self.rank_num = rank # To handle winning comparison 
 
     def __str__(self):
-        return "{} of {}".format(self.rank_num,self.suit) 
+        return "{} of {}".format(self.rank,self.suit) 
 
 class Deck(object):
     def __init__(self): # Don't need any input to create a deck of cards
